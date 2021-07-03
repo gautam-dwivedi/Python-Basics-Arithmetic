ea1ee021fe60e085b28550e06f049a711f320dd1
Gautam Dwivedi <79519946+gautam-dwivedi@users.noreply.github.com>
2021-07-03 14:44:42 +0530

    Add files via upload

---
+++ 05 arithmetic1.py
@@ -0,0 +1,13 @@
+
+# Python Basics
+
+print("enter no a")
+a= input()
+
+print("enter no b")
+b= input()
+
+Division = int(a) / int(b)
+print (a)
+print("Division is:", Division)
+print("type",type(Division))
---
+++ 06 arithmetic2.py
@@ -0,0 +1,14 @@
+
+# Python Basics
+
+print("enter no a")
+a= input()
+#a= int (input("enter the value of a\n"))
+
+print("enter no b")
+b= input()
+#b= int(input("enter the value of b\n"))
+
+addition = a + b
+print("Addition is:", addition)
+print("type",type(addition))

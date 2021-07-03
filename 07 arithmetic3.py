5dbf5c5448ed39d4ef9cd54e89f093d3caa74781
Gautam Dwivedi <79519946+gautam-dwivedi@users.noreply.github.com>
2021-07-03 14:45:10 +0530

    Add files via upload

---
+++ 07 arithmetic3.py
@@ -0,0 +1,12 @@
+
+# Python Basics
+
+print("enter no a")
+a= input()
+
+print("enter no b")
+b= input()
+
+addition = int(a) +  int(b)   #typecasting
+
+print("addition is:", addition)
---
+++ 08 arithmetic4.py
@@ -0,0 +1,12 @@
+
+
+# Python Basic
+
+print("enter no a")
+a= int(input())
+print("enter no b")
+b= int(input())
+
+addition = a + b
+print("addition is:", addition)
+print("type",type(addition))
